930e03d17b47e6c19e7bf7922416c0a230d640a4
Guo-MengTing <515572944@qq.com>
2019-07-08 21:00:01 +0800

    练习

---
+++ 7-8-2.cpp
@@ -0,0 +1,27 @@
+//https://www.nowcoder.com/questionTerminal/0f6976af36324f8bab1ea61e9e826ef5
+//#include<iostream>
+//#include<math.h>
+//using namespace std;
+//int main()
+//{
+//	int num;
+//	while (cin >> num)
+//	{
+//		int i = 2;
+//		cout << num << " = ";
+//		for (i; i <= sqrt(num); ++i)
+//		{
+//			while (num != i)
+//			{
+//				if (num%i == 0)
+//				{
+//					cout << i << " * ";
+//					num = num / i;
+//				}
+//				else
+//					break;
+//			}
+//		}
+//		cout << num << endl;
+//	}
+//}
